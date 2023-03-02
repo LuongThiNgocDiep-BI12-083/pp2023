3729b1b6d9c77d74606b26eeb0e93bc9754c50b9
ngocdiep083 <diepltn.bi12-083@st.usth.edu.vn>
2023-03-02 19:55:24 +0700

    Exercise 2

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,55 +1,72 @@
-class Student:
+class Person:
     def __init__(self, id, name, dob):
         self.id = id
         self.name = name
         self.dob = dob
-        self.marks = {}
-    def add_marks(self, course_id, mark):
-        self.marks[course_id] = mark
+        
     def __str__(self):
         return f"{self.id} - {self.name} ({self.dob})"
+        
+class Student(Person):
+    def __init__(self, id, name, dob):
+        super().__init__(id, name, dob)
+        self.marks = {}
+        
+    def add_mark(self, course_id, mark):
+        self.marks[course_id] = mark
+        
+    def __str__(self):
+        return super().__str__()
 
 class Course:
     def __init__(self, id, name):
         self.id = id
         self.name = name
         self.students = []
+        
     def add_student(self, student):
         self.students.append(student)
+        
     def __str__(self):
         return f"{self.id} - {self.name}"
 
 class School:
-    def __init__(self):
-        self.students = {}
-        self.courses = {}
+    def __init__(self, students, courses):
+        self.students = students
+        self.courses = courses
+        
     def add_student(self, student):
         self.students[student.id] = student
+        
     def add_course(self, course):
         self.courses[course.id] = course
-    def add_marks(self, student_id, course_id, mark):
-        if student_id in self.students and course_id in self.courses:
-            student = self.students[student_id]
-            student.add_marks(course_id, mark)
-        else:
-            raise ValueError("Invalid student or course ID")
+        
+    def add_mark(self, student_id, course_id, mark):
+        for student in self.students:
+            if student.id == student_id:
+                student.add_mark(course_id, mark)
+                return
+        raise ValueError("Invalid student ID")
+        
     def list_students(self):
         print("List of students:")
-        for student in self.students.values():
+        for student in self.students:
             print(student)
+            
     def list_courses(self):
         print("List of courses:")
-        for course in self.courses.values():
+        for course in self.courses:
             print(course)
+            
     def show_marks(self, course_id):
-        if course_id in self.courses:
-            course = self.courses[course_id]
-            print(f"Marks for course: {course}")
-            for student in course.students:
-                marks = student.marks.get(course_id, "N/A")
-                print(f"{student}: {marks}")
-        else:
-            raise ValueError("Invalid course ID")
+        for course in self.courses:
+            if course.id == course_id:
+                print(f"Marks for course: {course}")
+                for student in course.students:
+                    marks = student.marks.get(course_id, "N/A")
+                    print(f"{student}: {marks}")
+                return
+        raise ValueError("Invalid course ID")
 
 if __name__ == '__main__':
     num_students = int(input("Enter the number of students: "))
@@ -70,18 +87,23 @@ if __name__ == '__main__':
         name = input("Enter course name: ")
         course = Course(id, name)
         courses.append(course)
+
     for student in students:
         for course in courses:
             course.add_student(student)
-        print("Select a course:")
+
+    print("Select a course:")
     for i, course in enumerate(courses):
         print(f"{i+1}. {course}")
-    course_index = int(input()) - 1
-    course = courses[course_index]
-    
+        course_index = int(input()) - 1
+        course = courses[course_index]
+
+    school = School(students, courses)
+
     for student in course.students:
-        mark = float(input(f"Enter mark for {student.name}: "))
-        School.add_marks(student.id, course.id, mark)
-    School.list_courses()
-    School.list_students()
-    School.show_marks(course.id)
+        mark = input(f"Enter mark for {student.name}: ")
+        school.add_mark(student.id, course.id, mark)
+
+    school.list_courses()
+    school.list_students()
+    school.show_marks(course.id)
